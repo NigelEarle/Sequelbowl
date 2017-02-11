1089902983de9bda72fe645aae937536005be1f8
nwre87 <nwre87@gmail.com>
2017-02-10 21:59:53 -1000

    change to team_id

--- models/Team.js
+++ models/Team.js
@@ -10,7 +10,7 @@ module.exports = function(sequelize, DataTypes) {
           foreignKey: 'team_id',
         });
         Team.hasMany(models.Coach, {
-          as: 'team_id'
+          foreignKey: 'team_id'
         })
       }
     }
